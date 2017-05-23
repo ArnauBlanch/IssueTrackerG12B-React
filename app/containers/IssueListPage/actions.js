c96440b507b97719b8ee9e4f0ace637163f47724
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-23 17:30:23 +0200

    Issue list page request

--- app/containers/IssueListPage/actions.js
+++ app/containers/IssueListPage/actions.js
@@ -5,11 +5,34 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  GET_ISSUES_REQUEST,
+  GET_ISSUES_SUCCESS,
+  GET_ISSUES_FAILURE,
+  CURRENTLY_SENDING,
 } from './constants';
 
-export function defaultAction() {
+export function getIssuesRequest() {
   return {
-    type: DEFAULT_ACTION,
+    type: GET_ISSUES_REQUEST,
+  };
+}
+
+export function getIssuesSuccess(issues) {
+  return {
+    type: GET_ISSUES_SUCCESS,
+    issues,
+  };
+}
+
+export function getIssuesFailure() {
+  return {
+    type: GET_ISSUES_FAILURE,
+  };
+}
+
+export function currentlySending(sending) {
+  return {
+    type: CURRENTLY_SENDING,
+    sending,
   };
 }
--- app/containers/IssueListPage/constants.js
+++ app/containers/IssueListPage/constants.js
@@ -4,4 +4,7 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/IssueListPage/DEFAULT_ACTION';
+export const GET_ISSUES_REQUEST = 'app/IssueListPage/GET_ISSUES_REQUEST';
+export const GET_ISSUES_SUCCESS = 'app/IssueListPage/GET_ISSUES_SUCCESS';
+export const GET_ISSUES_FAILURE = 'app/IssueListPage/GET_ISSUES_FAILURE';
+export const CURRENTLY_SENDING = 'app/IssueListPage/CURRENTLY_SENDING';
