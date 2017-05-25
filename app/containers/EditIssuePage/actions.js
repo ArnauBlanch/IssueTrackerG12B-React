5754f7f117ff0b3ef3d2242ae04100f3bc9c0bbb
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-25 02:33:53 +0200

    Edit issue sagas&actions started + handle and prepare form submission

--- app/containers/EditIssuePage/actions.js
+++ app/containers/EditIssuePage/actions.js
@@ -7,6 +7,9 @@
 import {
   GET_USERS_REQUEST,
   GET_USERS_SUCCESS,
+  EDIT_ISSUE_REQUEST,
+  EDIT_ISSUE_SUCCESS,
+  EDIT_ISSUE_FAILURE,
 } from './constants';
 
 export function getUsersRequest() {
@@ -15,4 +18,16 @@ export function getUsersRequest() {
 
 export function getUsersSuccess(users) {
   return { type: GET_USERS_SUCCESS, users };
+}
+
+export function editIssueRequest(id, newValues) {
+  return { type: EDIT_ISSUE_REQUEST, id, newValues };
+}
+
+export function editIssueSuccess() {
+  return { type: EDIT_ISSUE_SUCCESS };
+}
+
+export function editIssueFailure(error) {
+  return { type: EDIT_ISSUE_FAILURE, error };
 }
