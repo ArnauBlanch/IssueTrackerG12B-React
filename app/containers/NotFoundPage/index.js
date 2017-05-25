42977efe0298f779c58f5524776bb719a8c03107
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-25 02:50:58 +0200

    IssueForm fix + error 404 page

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -10,16 +10,15 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
+import { blue800 } from 'material-ui/styles/colors';
 
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ fontSize: 80, color: blue800 }}>Ooops!</div>
+        <div style={{ marginTop: 60, fontSize: 18 }}>The page you were looking for does not exist...</div>
+      </div>
     );
   }
 }
