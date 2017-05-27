92f59613f15fe7f2b0cd214e8308754e13c4cf8f
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-27 20:29:15 +0200

    Vote and unvote actions added

--- app/containers/IssueDetailsPage/sagas.js
+++ app/containers/IssueDetailsPage/sagas.js
@@ -1,6 +1,6 @@
 import { take, call, put, fork } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
-import { GET_ISSUE_REQUEST, DELETE_COMMENT } from './constants';
+import { GET_ISSUE_REQUEST, DELETE_COMMENT, VOTE_ISSUE, WATCH_ISSUE } from './constants';
 import { getIssueRequest, getIssueSuccess, getIssueFailure, currentlySending } from './actions';
 import request from '../../utils/request';
 
@@ -32,9 +32,35 @@ export function* deleteComment() {
   }
 }
 
+export function* voteIssue() {
+  while(true) { // eslint-disable-line
+    const { id } = yield take(VOTE_ISSUE);
+    const response = yield call(request, `/issues/${id}/vote`, 'POST', undefined, false);
+    if (response.status === 200) {
+      yield put(push(`/issues/${id}`));
+    } else if (response.status === 404) {
+      yield put(push('/issue-not-found'));
+    }
+  }
+}
+
+export function* watchIssue() {
+  while(true) { // eslint-disable-line
+    const { id } = yield take(WATCH_ISSUE);
+    const response = yield call(request, `/issues/${id}/watch`, 'POST', undefined, false);
+    if (response.status === 200) {
+      yield put(push(`/issues/${id}`));
+    } else if (response.status === 404) {
+      yield put(push('/issue-not-found'));
+    }
+  }
+}
+
 export function* issueDetailsSaga() {
   yield fork(getIssue);
   yield fork(deleteComment);
+  yield fork(voteIssue);
+  yield fork(watchIssue);
 }
 
 export default [
