73a85911a30f203c1ed9cf1ed514ebd3a148a4ae
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-28 17:55:11 +0200

    Filters done

--- app/containers/IssueListPage/index.js
+++ app/containers/IssueListPage/index.js
@@ -10,12 +10,18 @@ import { push } from 'react-router-redux';
 import Helmet from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import { FloatingActionButton, Card, CircularProgress } from 'material-ui';
+import { FloatingActionButton, Card, CircularProgress, RaisedButton } from 'material-ui';
 import { getIssuesRequest } from './actions';
 import makeSelectIssueListPage, { makeSelectAuthState } from './selectors';
 import IssuesTable from '../../components/IssuesTable';
+import ApiUsers from '../../utils/ApiUsers';
 
 class IssueListPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = { currentFilter: 'open' };
+  }
+
   componentWillMount() {
     this.props.dispatch(getIssuesRequest());
   }
@@ -26,8 +32,28 @@ class IssueListPage extends React.Component { // eslint-disable-line react/prefe
     }
   }
 
+  filterOpen() {
+    this.setState({ currentFilter: 'open' });
+    this.render();
+  }
+
+  filterAll() {
+    this.setState({ currentFilter: 'all' });
+    this.render();
+  }
+
+  filterMyIssues() {
+    this.setState({ currentFilter: 'my_issues' });
+    this.render();
+  }
+
+  filterWatching() {
+    this.setState({ currentFilter: 'watching' });
+    this.render();
+  }
+
   render() {
-    const { issues, currentlySending } = this.props.issuesState;
+    let { issues, currentlySending } = this.props.issuesState;
     const cardStyle = {
       margin: 10,
       maxWidth: 1000,
@@ -35,6 +61,31 @@ class IssueListPage extends React.Component { // eslint-disable-line react/prefe
       paddingRight: 10,
       marginBottom: 60,
     };
+
+    function openIssues(issue) {
+      return issue.status === 'open';
+    }
+
+    function myIssues(issue) {
+      if (issue._links.assignee) {
+        return issue._links.assignee.nickname === ApiUsers[localStorage.getItem('authUser')].nickname;
+      }
+      return false;
+    }
+
+    function watchingIssues(issue) {
+      return issue.watched_by_current_user === true;
+    }
+
+    if (!currentlySending) {
+      if (this.state.currentFilter === 'open') {
+        issues = issues.filter(openIssues);
+      } else if (this.state.currentFilter === 'my_issues') {
+        issues = issues.filter(myIssues);
+      } else if (this.state.currentFilter === 'watching') {
+        issues = issues.filter(watchingIssues);
+      }
+    }
 
     return (
       <div>
@@ -45,10 +96,48 @@ class IssueListPage extends React.Component { // eslint-disable-line react/prefe
           ]}
         />
         { currentlySending || !issues ?
-          <CircularProgress size={60} thickness={6} />
-          : <Card style={cardStyle}>
-            <IssuesTable issues={issues} />
-          </Card>
+          <CircularProgress size={60} thickness={6} /> :
+          <div>
+            <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--6-col" style={{ marginLeft: '0px' }}>
+                <RaisedButton
+                  label="All"
+                  labelStyle={{ fontWeight: 'bold' }}
+                  onTouchTap={() => this.filterAll()}
+                  primary={this.state.currentFilter === 'all'}
+                />
+                <RaisedButton
+                  label="Open" style={{ marginLeft: '15px' }}
+                  labelStyle={{ fontWeight: 'bold' }}
+                  onTouchTap={() => this.filterOpen()}
+                  primary={this.state.currentFilter === 'open'}
+                />
+                {
+                  this.props.authState.isAuthenticated &&
+                  <RaisedButton
+                    label="My issues"
+                    style={{ marginLeft: '15px' }}
+                    labelStyle={{ fontWeight: 'bold' }}
+                    onTouchTap={() => this.filterMyIssues()}
+                    primary={this.state.currentFilter === 'my_issues'}
+                  />
+                }
+                {
+                  this.props.authState.isAuthenticated &&
+                  <RaisedButton
+                    label="Watching"
+                    style={{ marginLeft: '15px' }}
+                    labelStyle={{ fontWeight: 'bold' }}
+                    onTouchTap={() => this.filterWatching()}
+                    primary={this.state.currentFilter === 'watching'}
+                  />
+                }
+              </div>
+            </div>
+            <Card style={cardStyle}>
+              <IssuesTable issues={issues} />
+            </Card>
+          </div>
         }
         { this.props.authState.isAuthenticated &&
           <FloatingActionButton
